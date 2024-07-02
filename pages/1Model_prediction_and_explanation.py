fda3159ffa1e6840176d534fd30d049c88e4deaa
Aditya Panchal <50858157+adityapanchal10@users.noreply.github.com>
2024-07-02 23:42:28 +0200

    Update 1Model_prediction_and_explanation.py

--- pages/1Model_prediction_and_explanation.py
+++ pages/1Model_prediction_and_explanation.py
@@ -82,7 +82,7 @@ def display_shap_summary_plot(explainer, shap_values, X, y):
         clust = st.session_state['clustering']
     else:
         clust = shap.utils.hclust(X, y, linkage="single")
-        st.session_state['clustering'] = clustering
+        st.session_state['clustering'] = clust
     shap.plots.bar(shap_values, clustering=clust, clustering_cutoff=2, max_display=len(X.columns))
     st.pyplot()
     plt.clf()  # Clear the current figure after displaying it
